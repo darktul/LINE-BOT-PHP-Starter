7431f0c370860fcd7bf5e6d46852dcb69f77c3a4
darktul <jariwat_tul_@hotmail.com>
2017-09-26 23:48:10 +0700

    update help condition

--- bot.php
+++ bot.php
@@ -26,12 +26,12 @@ if (!is_null($events['events'])) {
 				'text' => $text
 			];
 
-			$messages3 = [
+			$messages2 = [
 			  "type"=> "template",
 			  "altText"=> "this is a confirm template",
 			  "template"=> [
 			      "type"=> "confirm",
-			      "text"=> "Are you sure?",
+			      "text"=> "Are you sure to join queue?",
 			      "actions"=> [
 			          [
 			            "type"=> "message",
@@ -47,8 +47,15 @@ if (!is_null($events['events'])) {
 			  ]	
 			];	
 
-			if($text != "reservation"){
+			// Build message to reply back
+			$messages3 = [
+				'type' => 'text',
+				// 'text' => $text
+				'text' => "reservation - join queue<br/>show - show queue status<br/>quit - quit queue"
+			];
+
 			// Make a POST Request to Messaging API to reply to sender
+			if($text != "join"){
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
@@ -71,8 +78,33 @@ if (!is_null($events['events'])) {
 			echo $result . "\r\n";
 			}
 	
-			if($text == "reservation"){
 			// Make a POST Request to Messaging API to reply to sender
+			if($text == "join"){
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages2],
+			];
+
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			curl_setopt($ch, CURLOPT_PROXY, $proxy);
+			curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+			}
+
+			// Make a POST Request to Messaging API to reply to sender
+			if($text == "help"){
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
@@ -97,61 +129,5 @@ if (!is_null($events['events'])) {
 			}
 		}
 	}
-
-	// // Loop through each event
-	// foreach ($events['events'] as $event) {
-	// 	// Reply only when message sent is in 'text' format
-	// 	if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-	// 		// Get text sent
-	// 		$text = $event['message']['text'];
-	// 		// Get replyToken
-	// 		$replyToken = $event['replyToken'];
-	// 		// Build message to reply back
-	// 		$messages3 = [
-	// 		  "type"=> "template",
-	// 		  "altText"=> "this is a confirm template",
-	// 		  "template"=> [
-	// 		      "type"=> "confirm",
-	// 		      "text"=> "Are you sure?",
-	// 		      "actions"=> [
-	// 		          [
-	// 		            "type"=> "message",
-	// 		            "label"=> "Yes",
-	// 		            "text"=> "yes"
-	// 		          ],
-	// 		          [
-	// 		            "type"=> "message",
-	// 		            "label"=> "No",
-	// 		            "text"=> "no"
-	// 		          ]
-	// 		      ]
-	// 		  ]
-			
-	// 		];		
-	// 		if($text == "reservation"){
-	// 		// Make a POST Request to Messaging API to reply to sender
-	// 		$url = 'https://api.line.me/v2/bot/message/reply';
-	// 		$data = [
-	// 			'replyToken' => $replyToken,
-	// 			'messages' => [$messages3],
-	// 		];
-	// 		$post = json_encode($data);
-	// 		$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-	// 		$ch = curl_init($url);
-	// 		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-	// 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	// 		curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-	// 		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-	// 		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-	// 		curl_setopt($ch, CURLOPT_PROXY, $proxy);
-	// 		curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
-	// 		$result = curl_exec($ch);
-	// 		curl_close($ch);
-
-	// 		echo $result . "\r\n";
-	// 		}
-	// 	}
-	// }
 }
 echo "OK";
