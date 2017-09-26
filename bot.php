34a4f828f4815e88a2e638541af63b55df5e5a54
darktul <jariwat_tul_@hotmail.com>
2017-09-26 17:45:40 +0700

    test confirm request from LINE API

--- bot.php
+++ bot.php
@@ -66,6 +66,24 @@ if (!is_null($events['events'])) {
 				'text' => "tul"
 			];
 
+			$messages2 = [
+				'type' => 'confirm',
+				// 'text' => $text
+				'text' => "Are you sure?"
+				'actions' => [          
+				{
+		            "type": "message",
+		            "label": "Yes",
+		            "text": "yes"
+		          },
+		          {
+		            "type": "message",
+		            "label": "No",
+		            "text": "no"
+		          }
+		      	]
+			];
+
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
