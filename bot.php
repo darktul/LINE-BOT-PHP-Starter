11cd74269c71253e0fd4a23f45dbf06570ada18b
darktul <jariwat_tul_@hotmail.com>
2017-09-26 23:29:44 +0700

    Final test reservation condition

--- bot.php
+++ bot.php
@@ -26,62 +26,15 @@ if (!is_null($events['events'])) {
 				'text' => $text
 			];
 
-			if($text != "reservation"){
+			if($text != "reservation"){{
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
 				'messages' => [$messages],
 			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			curl_setopt($ch, CURLOPT_PROXY, $proxy);
-			curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";
 			}
-		}
-	}
 
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['message']['text'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
-			// Build message to reply back
-			$messages3 = [
-			  "type"=> "template",
-			  "altText"=> "this is a confirm template",
-			  "template"=> [
-			      "type"=> "confirm",
-			      "text"=> "Are you sure?",
-			      "actions"=> [
-			          [
-			            "type"=> "message",
-			            "label"=> "Yes",
-			            "text"=> "yes"
-			          ],
-			          [
-			            "type"=> "message",
-			            "label"=> "No",
-			            "text"=> "no"
-			          ]
-			      ]
-			  ]
-			
-			];		
 			if($text == "reservation"){
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
@@ -89,6 +42,7 @@ if (!is_null($events['events'])) {
 				'replyToken' => $replyToken,
 				'messages' => [$messages3],
 			];
+			}
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
@@ -107,5 +61,61 @@ if (!is_null($events['events'])) {
 			}
 		}
 	}
+
+	// // Loop through each event
+	// foreach ($events['events'] as $event) {
+	// 	// Reply only when message sent is in 'text' format
+	// 	if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+	// 		// Get text sent
+	// 		$text = $event['message']['text'];
+	// 		// Get replyToken
+	// 		$replyToken = $event['replyToken'];
+	// 		// Build message to reply back
+	// 		$messages3 = [
+	// 		  "type"=> "template",
+	// 		  "altText"=> "this is a confirm template",
+	// 		  "template"=> [
+	// 		      "type"=> "confirm",
+	// 		      "text"=> "Are you sure?",
+	// 		      "actions"=> [
+	// 		          [
+	// 		            "type"=> "message",
+	// 		            "label"=> "Yes",
+	// 		            "text"=> "yes"
+	// 		          ],
+	// 		          [
+	// 		            "type"=> "message",
+	// 		            "label"=> "No",
+	// 		            "text"=> "no"
+	// 		          ]
+	// 		      ]
+	// 		  ]
+			
+	// 		];		
+	// 		if($text == "reservation"){
+	// 		// Make a POST Request to Messaging API to reply to sender
+	// 		$url = 'https://api.line.me/v2/bot/message/reply';
+	// 		$data = [
+	// 			'replyToken' => $replyToken,
+	// 			'messages' => [$messages3],
+	// 		];
+	// 		$post = json_encode($data);
+	// 		$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+	// 		$ch = curl_init($url);
+	// 		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+	// 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	// 		curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+	// 		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	// 		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	// 		curl_setopt($ch, CURLOPT_PROXY, $proxy);
+	// 		curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+	// 		$result = curl_exec($ch);
+	// 		curl_close($ch);
+
+	// 		echo $result . "\r\n";
+	// 		}
+	// 	}
+	// }
 }
 echo "OK";
