0754ee648653d6cb4fd611fd26312e22a069c7f2
darktul <jariwat_tul_@hotmail.com>
2017-09-26 23:32:16 +0700

    no message

--- bot.php
+++ bot.php
@@ -26,6 +26,27 @@ if (!is_null($events['events'])) {
 				'text' => $text
 			];
 
+			$messages3 = [
+			  "type"=> "template",
+			  "altText"=> "this is a confirm template",
+			  "template"=> [
+			      "type"=> "confirm",
+			      "text"=> "Are you sure?",
+			      "actions"=> [
+			          [
+			            "type"=> "message",
+			            "label"=> "Yes",
+			            "text"=> "yes"
+			          ],
+			          [
+			            "type"=> "message",
+			            "label"=> "No",
+			            "text"=> "no"
+			          ]
+			      ]
+			  ]	
+			];	
+
 			if($text != "reservation"){{
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
@@ -43,6 +64,7 @@ if (!is_null($events['events'])) {
 				'messages' => [$messages3],
 			];
 			}
+
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
