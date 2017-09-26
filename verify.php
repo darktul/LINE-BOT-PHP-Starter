853279636034a8f8e6061b30c12dd80c906553e9
darktul <jariwat_tul_@hotmail.com>
2017-09-26 12:27:07 +0700

    Add files via upload
    update curl

--- verify.php
+++ verify.php
@@ -1,6 +1,7 @@
 <?php
 $proxy = 'http://fixie:3Zo31wkG9rMPtyf@velodrome.usefixie.com:80';
 $proxyauth = 'fixie:3Zo31wkG9rMPtyf';
+
 $access_token = 'DWLT+dALUA6eDgEc03psgiSF/z4hGt1hjAizvr5RHWs504y/Fu70qEEsu9cY1NjLbJ8/mju46Z6K22+glLgYO0ELhd175QMLTatEChiUbS2/JPjiLt5Vcnn351jd6AjT7IWk4NmOOajJURKIlx77FAdB04t89/1O/w1cDnyilFU=
 ';
 
@@ -12,6 +13,8 @@ $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_PROXY, $proxy);
+curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
 $result = curl_exec($ch);
 curl_close($ch);
 
