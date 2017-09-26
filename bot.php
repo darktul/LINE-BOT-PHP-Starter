0c7bbc553a52fb94e1f0eb82b954842ffda463fb
darktul <jariwat_tul_@hotmail.com>
2017-09-26 23:17:11 +0700

    test reservation condition

--- bot.php
+++ bot.php
@@ -14,12 +14,50 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text' && $text != "reservation") {
 			// Get text sent
 			$text = $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				// 'text' => $text
+				'text' => $text
+			];
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			curl_setopt($ch, CURLOPT_PROXY, $proxy);
+			curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+		}
+	}
+
+	// Loop through each event
+	foreach ($events['events'] as $event) {
+		// Reply only when message sent is in 'text' format
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text' && $text == "reservation") {
+			// Get text sent
+			$text = $event['message']['text'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+			// Build message to reply back
 			$messages3 = [
 			  "type"=> "template",
 			  "altText"=> "this is a confirm template",
@@ -40,32 +78,7 @@ if (!is_null($events['events'])) {
 			      ]
 			  ]
 			
-			];
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				// 'text' => $text
-				'text' => "tul"
-			];
-
-			// $messages2 = [
-			// 	'type' => 'confirm',
-			// 	// 'text' => $text
-			// 	'text' => "Are you sure?"
-			// 	'actions' => [          
-			// 	{
-		 //            'type'=> 'message',
-		 //            'label'=> 'Yes',
-		 //            'text'=> "yes"
-		 //          },
-		 //          {
-		 //            'type'=> 'message',
-		 //            'label'=> 'No',
-		 //            'text'=> "no"
-		 //          }
-		 //      	]
-			// ];
-
+			];		
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
